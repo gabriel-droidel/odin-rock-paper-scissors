a09bcc0653e349b90678a04c2f756edded1e845e
gabi droid <193822798+gabriel-droidel@users.noreply.github.com>
2025-01-08 19:43:38 +0100

    Finish main game loop

--- script.js
+++ script.js
@@ -13,12 +13,13 @@ function getComputerChoice(){
         case 3:
             return 'scissors';
     }
+    console.log(getComputerChoice());
 }
 
-console.log(getComputerChoice());
+
 
 function getPlayerChoice(){
-    const userChoice = prompt('Choose Rock, Paper, Scissors');
+    const userChoice = prompt('Choose Rock, Paper, Scissors').toLowerCase();
 
     switch (userChoice){
         case 'rock':
@@ -26,10 +27,41 @@ function getPlayerChoice(){
         case 'paper':
             return 'paper';
         case 'scissors':
-            return scissors;
+            return 'scissors';
     }
 }
 
-function playRound(computerSelection, userSelection){
-    
+function playRound(playerOne, playerTwo){
+    if (((playerOne==='rock')&&(playerTwo==='scissors'))
+    ||((playerOne==='scissors')&&(playerTwo==='paper'))
+    ||((playerOne==='paper')&&(playerTwo==='rock')))
+    {
+        return playerOne;
+    } else if (playerOne===playerTwo)
+        return 0;
+    else
+        return playerTwo;
+}
+
+function playGame(){
+    let playerOneScore=0, playerTwoScore=0;
+    for(let i=1;i<=5;i++){
+        getScore(playerOneScore,playerTwoScore);
+        console.log(`Round: ${i}`);
+        console.log(`Computer: ${playerOneScore}`);
+        console.log(`Player: ${playerTwoScore}`);
+    }
+
+}
+
+function getScore(playerOneScore, playerTwoScore){
+    let playerOneChoice=getComputerChoice();
+    let playerTwoChoice=getPlayerChoice();
+    let roundResult='';
+
+    roundResult=playRound(playerOneChoice,playerTwoChoice);
+    if(roundResult===playerOneChoice)
+        return playerOneScore++;
+    else if(roundResult===playerTwoChoice) 
+        return playerTwoScore++;
 }
