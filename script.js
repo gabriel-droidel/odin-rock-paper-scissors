0d1fbab140cb4de98a5a39588c119ae1ab7200de
gabi droid <193822798+gabriel-droidel@users.noreply.github.com>
2025-01-08 20:35:40 +0100

    Finish game logic in console

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Function that gets a random choice, rock paper or scissors.
-
-// Function that gets user input
 
 function getComputerChoice(){
     const randomNumber = Math.floor(Math.random() * 3) + 1; // Get random number from interval 1-3
@@ -13,10 +10,7 @@ function getComputerChoice(){
         case 3:
             return 'scissors';
     }
-    console.log(getComputerChoice());
 }
-
-
 
 function getPlayerChoice(){
     const userChoice = prompt('Choose Rock, Paper, Scissors').toLowerCase();
@@ -32,13 +26,14 @@ function getPlayerChoice(){
 }
 
 function playRound(playerOne, playerTwo){
+    
     if (((playerOne==='rock')&&(playerTwo==='scissors'))
     ||((playerOne==='scissors')&&(playerTwo==='paper'))
     ||((playerOne==='paper')&&(playerTwo==='rock')))
     {
         return playerOne;
     } else if (playerOne===playerTwo)
-        return 0;
+        console.log(`Draw!`);
     else
         return playerTwo;
 }
@@ -46,22 +41,28 @@ function playRound(playerOne, playerTwo){
 function playGame(){
     let playerOneScore=0, playerTwoScore=0;
     for(let i=1;i<=5;i++){
-        getScore(playerOneScore,playerTwoScore);
+        let playerOneChoice=getComputerChoice();
+        let playerTwoChoice=getPlayerChoice();
+        let roundResult='';
+        roundResult=playRound(playerOneChoice,playerTwoChoice);
+
+        if(roundResult===playerOneChoice)
+            playerOneScore++;
+        else if(roundResult===playerTwoChoice) 
+            playerTwoScore++;
+
         console.log(`Round: ${i}`);
         console.log(`Computer: ${playerOneScore}`);
         console.log(`Player: ${playerTwoScore}`);
     }
-
+    displayResults(playerOneScore,playerTwoScore);
 }
 
-function getScore(playerOneScore, playerTwoScore){
-    let playerOneChoice=getComputerChoice();
-    let playerTwoChoice=getPlayerChoice();
-    let roundResult='';
-
-    roundResult=playRound(playerOneChoice,playerTwoChoice);
-    if(roundResult===playerOneChoice)
-        return playerOneScore++;
-    else if(roundResult===playerTwoChoice) 
-        return playerTwoScore++;
+function displayResults(playerOneScore, playerTwoScore){
+    if(playerOneScore>playerTwoScore)
+        console.log(`The Computer Wins!`);
+    else if(playerTwoScore>playerOneScore)
+        console.log(`The Player Wins!`);
+    else if(playerOneScore===playerTwoScore)
+        console.log(`It is a draw!`);
 }
